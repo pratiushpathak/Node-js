6eee39e143b13c1896d425eaad8a6890f92416b7
pratiush pathak <pratiushpathak@gmail.com>
2025-05-12 16:59:59 +0530

    stoping event loop

--- lecture-3-First Node Server/app.js
+++ lecture-3-First Node Server/app.js
@@ -2,10 +2,13 @@ const http=require('http');
 function requestListener(req,res)
 {
   console.log(req);
+  process.exit();//stops  event loop
+ 
 
 }
 const server=http.createServer(requestListener);
-const port=3001;
+
+const port=3002;
 server.listen(port,()=>{
   console.log(`Server running on address http://localhost:${port}`)
 });
